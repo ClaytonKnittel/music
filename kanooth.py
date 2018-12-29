cef70de45d7c615b7f50cece52f8a72e37e16952
claytonknittel <c.j.knittel@wustl.edu>
2018-12-29 13:29:52 -0600

    dlx kanoodle

--- kanooth.py
+++ kanooth.py
@@ -1,3 +1,70 @@
+import pygame
+
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+screen.convert()
+pygame.display.set_caption('Kanoodle')
+
+
+pfill = .8
+
+
+class vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, v):
+        return vector(self.x + v.x, self.y + v.y)
+
+    def __sub__(self, v):
+        return vector(self.x - v.x, self.y - v.y)
+
+    def __repr__(self):
+        return '<' + str(self.x) + ', ' + str(self.y) + '>'
+
+    def quickstr(self):
+        return str(self.x) + ',' + str(self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        r = 31
+        r = 17 * r + self.x
+        r = 17 * r + self.y
+        return r
+
+    def astuple(self):
+        return (int(self.x), int(self.y))
+
+
+class board:
+
+    def __init__(self, posi, len, lon, w, h):
+        self.w = w
+        self.h = h
+        self.dim = [len, lon]
+        self.holes = []  # all the holes (just for visualization)
+        self.circles = []
+        for i in range(w):
+            for j in range(h):
+                self.circles.append([posi + vector(i * len / w + len / w / 2, j * lon / h + lon / h / 2), (0, 0, 0)])
+
+    def set(self, i, j, col):
+        self.circles[self.h * i + j][1] = col
+
+    def clear(self):
+        for i in range(self.w):
+            for j in range(self.h):
+                self.circles[self.h * i + j][1] = (0, 0, 0)
+
+    def draw(self, draw, screen):
+        rad = int(min(pfill * self.dim[0] / self.w, pfill * self.dim[1] / self.h) / 2)
+        for c, col in self.circles:
+            draw.circle(screen, col, c.astuple(), rad)
 
 
 class node:
@@ -21,46 +88,14 @@ class item:
     def __eq__(self, other):
         return other == self.name
 
-
-def parse_file(file_loc):
-    with open(file_loc, 'r') as file:
-        itms = ['_'] + file.readline().strip().split(' ')
-        items = [item(i, loc - 1, loc + 1) for i, loc in zip(itms, range(1, len(itms) + 1))]
-        items[0].left = len(items) - 1
-        items[-1].right = 0
-        nodes = [node(x, x, 0) for x in range(len(itms))]
-        p = len(nodes)
-
-        first_in_row = 0
-        nodes.append(node(first_in_row, 0, 0))
-        last_spacer = nodes[-1]
-        num_spacers = 1
-
-        for line in file.readlines():
-            first_in_row = 0
-            p += 1
-            for opt in line.strip().split(' '):
-                i = items.index(opt)
-                itm = nodes[i]
-                itm.aux += 1
-                nodes.append(node(itm.up, i, i))
-                nodes[itm.up].down = p
-                nodes[i].up = p
-                if first_in_row == 0:
-                    first_in_row = p
-                p += 1
-            last_spacer.down = p - 1
-
-            nodes.append(node(first_in_row, 0, -num_spacers))
-            last_spacer = nodes[-1]
-            num_spacers += 1
-        return items, nodes
+    def __repr__(self):
+        return 'n: {} l: {} r: {}   '.format(self.name, self.left, self.right)
 
 
 def hide(nodes, p):
     q = p + 1
     while q != p:
-        x = nodes[q].top
+        x = nodes[q].aux
         u = nodes[q].up
         d = nodes[q].down
         if x <= 0:
@@ -86,7 +121,7 @@ def cover(nodes, items, i):
 def unhide(nodes, p):
     q = p - 1
     while q != p:
-        x = nodes[q].top
+        x = nodes[q].aux
         u = nodes[q].up
         d = nodes[q].down
         if x <= 0:
@@ -109,16 +144,126 @@ def uncover(nodes, items, i):
         p = nodes[p].up
 
 
-def selp(nodes, items, l):
-    pass
+def get_index(nodes, xl):
+    # gets the index of the option containing node xl
+    while nodes[xl].aux > 0:
+        xl -= 1
+    return -nodes[xl].aux
+
+
+colormap = {'w0': (255,0,255), 'w1': (0,0,255), 'w2': (255,127,0), 'w3': (127,220,255), 'w4': (255,0,0), 'w5': (153,153,153),
+'w6': (255,105,181), 'w7': (0,180,26), 'w8': (.230,193,193), 'w9': (0, 255, 0), 'w10': (255, 255, 0), 'w11': (255,180,180)}
+
+b = board(vector(0, 0), 640, 480, 11, 5)
+
+
+def create_board_setup(soln):
+    b.clear()
+    for option in soln:
+        col = colormap[option[0]]
+        for tile in option[1:]:
+            i, j = tile.split(',')
+            x = int(i)
+            y = int(j)
+            b.set(x, y, col)
+
+
+def selp(nodes, items, options, partial_soln):
+    if items[0].right == 0:
+        yield [options[xl] for xl in partial_soln]
+        return
+    min_seen = -1
+    min_index = 0
+    find = items[0].right
+    while find != 0:
+        m = nodes[find].aux
+        if m < min_seen or min_seen == -1:
+            min_seen = m
+            min_index = find
+        find = items[find].right
+
+    i = min_index
+    cover(nodes, items, i)
+    xl = nodes[i].down
+    while xl != i:
+        p = xl + 1
+        while p != xl:
+            j = nodes[p].aux
+            if j <= 0:
+                p = nodes[p].up
+            else:
+                cover(nodes, items, j)
+                p += 1
+        part = partial_soln + [get_index(nodes, xl)]
+        # yield [options[xl] for xl in part]
+        yield from selp(nodes, items, options, part)
+        p = xl - 1
+        while p != xl:
+            j = nodes[p].aux
+            if j <= 0:
+                p = nodes[p].down
+            else:
+                uncover(nodes, items, j)
+                p -= 1
+        xl = nodes[xl].down
+    uncover(nodes, items, i)
+
+
+def parse_file(file_loc):
+    with open(file_loc, 'r') as file:
+        itms = ['_'] + file.readline().strip().split(' ')
+        items = [item(i, loc - 1, loc + 1) for i, loc in zip(itms, range(len(itms)))]
+        items[0].left = len(items) - 1
+        items[-1].right = 0
+        nodes = [node(x, x, 0) for x in range(len(itms))]
+        p = len(nodes)
+
+        first_in_row = 0
+        nodes.append(node(first_in_row, 0, 0))
+        last_spacer = nodes[-1]
+        num_spacers = 1
+        options = []
+
+        for line in file.readlines():
+            first_in_row = 0
+            p += 1
+            options.append(line.strip().split(' '))
+            for opt in options[-1]:
+                i = items.index(opt)
+                itm = nodes[i]
+                itm.aux += 1
+                nodes.append(node(itm.up, i, i))
+                nodes[itm.up].down = p
+                nodes[i].up = p
+                if first_in_row == 0:
+                    first_in_row = p
+                p += 1
+            last_spacer.down = p - 1
+
+            nodes.append(node(first_in_row, 0, -num_spacers))
+            last_spacer = nodes[-1]
+            num_spacers += 1
+        return nodes, items, options
 
 
 def solve(file_loc):
-    items, nodes = parse_file('inpt.txt')
+    nodes, items, options = parse_file(file_loc)
+    yield from selp(nodes, items, options, [])
 
 
 if __name__ == '__main__':
-    items, nodes = parse_file('inpt.txt')
-    for i, l in enumerate(nodes):
-        print(i, l)
+    #solve('kan.txt')
+    run = 1
+    gen = solve('kan.txt')
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = 0
+        screen.fill((255, 255, 255))
+        if run == 1:
+            create_board_setup(next(gen))
+            run += 1
+        b.draw(pygame.draw, screen)
+        pygame.display.flip()
+    pygame.display.quit()
 
